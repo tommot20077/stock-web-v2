d2a156a87cb39fd6dad41a985428840ab18c8592
Yuan <tommot20077@gmail.com>
2026-03-21 21:06:39 +0800

    Initial commit

---
+++ src/main/java/dowob/xyz/stockwebv2/StockWebV2Application.java
@@ -0,0 +1,13 @@
+package dowob.xyz.stockwebv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockWebV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockWebV2Application.class, args);
+	}
+
+}
